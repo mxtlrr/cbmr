966d43253e382ac59c69f468a54a4e37ca1d6d0f
mxtlrr <ruson0914@gmail.com>
2025-03-07 19:53:53 -0600

    delegate match making to server and respond with JSON with match data

--- src/cbmr.go
+++ src/cbmr.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"strings"
@@ -21,7 +23,8 @@ type ConnClientData struct {
 
 var (
 	connectedClients []ConnClientData
-	matchInPlay      bool // Is there a match currently going on?
+	matchInPlay      bool  // Is there a match currently going on?
+	match_id         int64 = 0
 )
 
 func main() {
@@ -34,7 +37,6 @@ func main() {
 
 func connectClient(w http.ResponseWriter, r *http.Request) {
 	var currentClient ConnClientData
-	log.Printf("I have recieved a new client connecting to the server!")
 
 	// /connect?name=[name]
 	query, _ := url.ParseQuery(r.URL.RawQuery)
@@ -43,32 +45,50 @@ func connectClient(w http.ResponseWriter, r *http.Request) {
 	currentClient.playerName = name
 	currentClient.clientAddress = strings.Split(r.RemoteAddr, ":")[0]
 
+	// TODO: check if already exists
+
 	connectedClients = append(connectedClients, currentClient)
-	log.Printf("Finished connecting client to server! There are %d clients online\n", len(connectedClients))
-	log.Println(currentClient)
+	log.Printf("Finished connecting client %d to server!\n", len(connectedClients))
 }
 
-func isConnectedPlayer(playerName string) bool {
-	var r bool = false
-	for i := 0; i < len(connectedClients); i++ {
-		if connectedClients[i].playerName == playerName {
-			r = true
-		}
-	}
-	return r
+func generateSeed() int32 {
+	return rand.Int31n(((1 << 31) - 1))
 }
 
 func beginMatch(w http.ResponseWriter, r *http.Request) {
+	// start_match?player1=[player1]&category=[category]
 	query, _ := url.ParseQuery(r.URL.RawQuery)
-	log.Println(query)
 
 	player1 := query.Get("player1")
-	player2 := query.Get("player2")
+	category := query.Get("category")
 
-	// If either one or the other are not connected, do not continue,
-	// make sure a match cannot start.
-	if !isConnectedPlayer(player1) || !isConnectedPlayer(player2) {
+	// Randomly select a player from the list
+	if len(connectedClients) < 1 {
 		io.WriteString(w, "FAILURE")
 		return
 	}
+
+	player2 := connectedClients[rand.Intn(len(connectedClients))].playerName
+	for player2 == player1 {
+		player2 = connectedClients[rand.Intn(len(connectedClients))].playerName
+	}
+
+	log.Printf("Selected players for match: %s vs %s in %s%%.\n",
+		player1, player2, category)
+
+	/*
+			{
+			"id": [match_id],
+			"players": [
+				"player1": [player1],
+				"player2": [player2]
+			],
+			"seed":	    [generate_seed]
+			"category": [category]
+		}
+	*/
+	matchInPlay = true
+	io.WriteString(w, fmt.Sprintf("{\n\t\"id\": %d,\n\t\"players\": [\n\t\t\"player1\": \"%s\",\n\t\t\"player2\": \"%s\"\n\t],\n\t\"seed\": %d\n\t\"category\": %s\n}\n",
+		match_id, player1, player2, generateSeed(), category))
+	match_id++
 }
