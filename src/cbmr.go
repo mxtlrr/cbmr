8507d85b6278bcbd7b66da0fe9cb8dbe778d31a6
mxtlrr <ruson0914@gmail.com>
2025-03-08 08:31:42 -0600

    start parsing end of match information

--- src/cbmr.go
+++ src/cbmr.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -22,12 +23,22 @@ type ConnClientData struct {
 	/* playerElo  int */
 }
 
+const (
+	MATCH_IN_PLAY = iota
+	MATCH_FOREFEIT
+	MATCH_DRAW
+	PLAYER1_WIN
+	PLAYER2_WIN
+)
+
 type CurrentMatch struct {
 	player1  string
 	player2  string
 	seed     string
 	matchId  int32
 	category string
+
+	currentResult int8
 }
 
 var (
@@ -113,7 +124,7 @@ func beginMatch(w http.ResponseWriter, r *http.Request) {
 		seedChosen = goodSeeds[rand.Intn(len(goodSeeds))]
 	}
 
-	currentMatch = CurrentMatch{player1, player2, seedChosen, int32(match_id), category}
+	currentMatch = CurrentMatch{player1, player2, seedChosen, int32(match_id), category, MATCH_IN_PLAY}
 
 	io.WriteString(w, fmt.Sprintf("{\n\t\"id\": %d,\n\t\"players\": [\n\t\t\"player1\": \"%s\",\n\t\t\"player2\": \"%s\"\n\t],\n\t\"seed\": \"%s\"\n\t\"category\": %s\n}\n",
 		match_id, player1, player2, seedChosen, category))
@@ -121,8 +132,47 @@ func beginMatch(w http.ResponseWriter, r *http.Request) {
 }
 
 func match_info(w http.ResponseWriter, r *http.Request) {
-	if matchInPlay {
-		io.WriteString(w, fmt.Sprintf("{\n\t\"id\": %d,\n\t\"players\": [\n\t\t\"player1\": \"%s\",\n\t\t\"player2\": \"%s\"\n\t],\n\t\"seed\": \"%s\"\n\t\"category\": %s\n}\n",
-			currentMatch.matchId, currentMatch.player1, currentMatch.player2, currentMatch.seed, currentMatch.category))
+	// If the client GETs this route, then we just want to display a JSON object that displays what
+	// the match looks like.
+	if r.Method == http.MethodGet {
+		if matchInPlay {
+			io.WriteString(w, fmt.Sprintf("{\n\t\"id\": %d,\n\t\"players\": [\n\t\t\"player1\": \"%s\",\n\t\t\"player2\": \"%s\"\n\t],\n\t\"seed\": \"%s\"\n\t\"category\": %s\n}\n",
+				currentMatch.matchId, currentMatch.player1, currentMatch.player2, currentMatch.seed, currentMatch.category))
+		}
+		return
 	}
+
+	// If POST, then we've got something about the timing/match ending!
+	// We'll assume it's a JSON object of the form
+	/*
+		{
+			"match_result": "win" 							// Based off the player who sent it.
+			"match_time": 	"0:12.345",
+			"forefeit_opp": "[player2's name]" // Only used if match_result = forefeit.
+			"sender":			  "[player1's name]"
+		}
+	*/
+	if r.Method == http.MethodPost {
+		// Decode JSON message
+		var matchIf matchInfoPOST
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer r.Body.Close()
+		err = json.Unmarshal(body, &matchIf)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// JSON decoded, now we can parse!
+	}
+}
+
+type matchInfoPOST struct {
+	match_result string
+	match_time   string
+	forefeit_opp string
+	sender       string
 }
